6aaa07d2f13cea3d9503a5060527c31aa351bd77
Robert Jenson <6thofthewest@gmail.com>
2024-11-05 01:04:52 -0500

    Update npc-generator.py

--- npc-generator.py
+++ npc-generator.py
@@ -2,7 +2,7 @@ import random
 # introduction to the generator
 print("Intelligence, charisma, and strength are all on a scale from 1 to 10\n")
 amount = int(input("How many characters would you like? "))
-counter = 1
+counter = 0
 
 # Attributes
 first_name = ["Chris", "Joe", "Elissa", "Sarah", "Alex", "Edward", "Karl", "Melvin", "Kim", "Nicholas", "Lilly", "Jack", "Tom", "Julia", "Hank", "Zoe", "Makenzie", "Steve", "Dennis", "Kate", "Peter", "George", "Mabel"]
@@ -14,7 +14,7 @@ strength = random.randint(1, 10)
 print("\n")
 
 # creates the amount of characters specified
-while counter <= amount:
+while counter < amount:
     print(random.choice(first_name), random.choice(last_name), f" - Age: {age}    Intelligence: {intelligence}    Charisma: {charisma}    Strength: {strength}")
     age = random.randint(1, 99)
     intelligence = random.randint(1, 10)
